2a2af7ee0693f96bcc5f52f16ea3e1968a59c9bf
Killllero0 <81145205+Killllero0@users.noreply.github.com>
2022-12-17 14:01:55 +0500

    add blogs model

--- Pr4/urls.py
+++ Pr4/urls.py
@@ -2,6 +2,9 @@
 Definition of urls for Pr4.
 """
 
+from django.conf.urls import include
+from django.contrib import admin
+
 from datetime import datetime
 from django.urls import path
 from django.contrib import admin
@@ -9,6 +12,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from app import forms, views
 
 
+admin.autodiscover();
 urlpatterns = [
     path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
@@ -28,4 +32,5 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('admin/', admin.site.urls),
     path('anketa/', views.anketa, name='anketa'),
+    path(r'^admin/', admin.site.urls),
 ]
--- app/models.py
+++ app/models.py
@@ -3,5 +3,28 @@ Definition of models.
 """
 
 from django.db import models
+from datetime import datetime
+from django.contrib import admin
+from django.urls import reverse
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=100, unique_for_date='posted', verbose_name='Заголовок')
+    description = models.TextField(verbose_name='Краткое содержание')
+    content = models.TextField(verbose_name='Полное содержание')
+    posted = models.DateTimeField(default=datetime.now(), db_index=True, verbose_name='Опубликована')
+
+    def get_absolute_url(self):
+        return reverse('blogpost', args=[str(self.id)])
+
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        db_table = 'Posts'
+        ordering = ['-posted']
+        verbose_name = 'статья блога'
+        verbose_name_plural = 'статьи блога'
+
+admin.site.register(Blog)
+
+
